750bdcac5013ef0b96f97c94ab03a80bde38007c
Nico <xeneizenico@gmail.com>
2022-02-19 19:06:56 -0300

    registro de visitas prueba

--- blog/views.py
+++ blog/views.py
@@ -49,40 +49,33 @@ def youtube(origin, id):
 # Create your views here.
 
 def prueba(request):
-    try:
-        #------------------------Visitor data------------------------------
-        
-        user_username = request.META['USERNAME']
-        
-        if user_username == 'xenei':
-            pass
-        else:
-            user_computername = request.META['COMPUTERNAME']
-            user_ip = requests.get('https://www.wikipedia.org').headers['X-Client-IP']
-            user_ip_data = requests.get('http://ip-api.com/json/{}'.format(user_ip)).json()
-            user_country = user_ip_data['country']
-            user_city = user_ip_data['regionName']
-            user_location = user_ip_data['city']
-            user_zip = user_ip_data['zip']
-            user_lat = user_ip_data['lat']
-            user_lon = user_ip_data['lon']
-            
-            #------------------------New visit------------------------------
-            
-            new_visit = Visit(
-                username=user_username,
-                computername=user_computername,
-                ip=user_ip,
-                country=user_country,
-                city=user_city,
-                location=user_location,
-                zip=user_zip,
-                lat=user_lat,
-                lon=user_lon
-            )
-            new_visit.save()
-    except:
-        pass
+    #------------------------Visitor data------------------------------
+    
+    user_username = request.META['USERNAME']
+    user_computername = request.META['COMPUTERNAME']
+    user_ip = requests.get('https://www.wikipedia.org').headers['X-Client-IP']
+    user_ip_data = requests.get('http://ip-api.com/json/{}'.format(user_ip)).json()
+    user_country = user_ip_data['country']
+    user_city = user_ip_data['regionName']
+    user_location = user_ip_data['city']
+    user_zip = user_ip_data['zip']
+    user_lat = user_ip_data['lat']
+    user_lon = user_ip_data['lon']
+    
+    #------------------------New visit------------------------------
+    
+    new_visit = Visit(
+        username=user_username,
+        computername=user_computername,
+        ip=user_ip,
+        country=user_country,
+        city=user_city,
+        location=user_location,
+        zip=user_zip,
+        lat=user_lat,
+        lon=user_lon
+    )
+    new_visit.save()
 
     tyc_news = []
     tyc_title = 'TyC Sports'
